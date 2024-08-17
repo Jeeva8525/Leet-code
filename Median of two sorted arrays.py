b5bda220f705f84c6c5a627524c8ba4d580e60b5
Jeeva8525 <jeevananthan8507@gmail.com>
2024-08-17 11:49:30 +0530

    Finds the median of two combined arrays

---
+++ Median of two sorted arrays.py
@@ -0,0 +1,7 @@
+print("Enter in the format of list like [1,2,3]")
+arr1=eval("Enter the 1st array: ")
+arr2=eval("Enter the 2nd array: ")
+arr=arr1+arr2
+arr.sort()
+print(arr)
+print((arr[0]+arr[-1])/2)
